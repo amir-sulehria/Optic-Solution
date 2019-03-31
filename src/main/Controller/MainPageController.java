46082050128760bb1d81a39658bb36b78a4c2f6b
Beautiful-Rainbow <m.amirsulehria@gmail.com>
2019-03-31 09:24:18 +0500

    Making Login Page Responsive

--- src/main/Controller/MainPageController.java
+++ src/main/Controller/MainPageController.java
@@ -1,9 +1,16 @@
 package main.Controller;
 
+import javafx.fxml.FXML;
+
 public class MainPageController {
 
     public void btnClick(){
         System.out.println("You clicked button");
+    }
+
+    @FXML
+    public void forgetPassword(){
+        //System.out.println("");
     }
 
 
